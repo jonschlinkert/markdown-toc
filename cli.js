6a85326ad3c52bf260def5b828e6087caa9ee6d8
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-21 03:54:57 -0500

    remove cli code
    will be introduced after I decide on approach

--- cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var path = require('path');
-var toc = require('./');
-
-var src = path.resolve(process.argv[2] || 'README.md');
-var dest = path.resolve(process.argv[3] || src);
-
-toc.add(src, dest);
--- example.js
+++
@@ -1,8 +0,0 @@
-
-var fs = require('fs');
-var toc = require('./');
-var content = fs.readFileSync('test/fixtures/basic.md', 'utf8');
-
-
-
-console.log(toc(content));
